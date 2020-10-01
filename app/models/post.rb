837f09c1c0d7c061a99f6ef5f455550252a18994
NerlyTon <nerlyton@gmail.com>
2020-10-01 14:28:29 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Post < ActiveRecord::Base
   has_many :post_categories
   has_many :categories, through: :post_categories
@@ -6,9 +7,15 @@ class Post < ActiveRecord::Base
   accepts_nested_attributes_for :categories
 
   def categories_attributes=(category_attributes)
+    # binding.pry
     category_attributes.values.each do |category_attribute|
+      # binding.pry
+      if category_attribute["name"].present?
       category = Category.find_or_create_by(category_attribute)
       self.categories << category
+      # if !self.categories.include?(category)
+      #   self.post_categories.build(:category => category)
+      end
     end
   end
 
